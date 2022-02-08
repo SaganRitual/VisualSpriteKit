1b19714246a5f7edd4ae0e4ab0277dbe529f3529
Rob Bishop <c.sagan.ritual@gmail.com>
2022-02-07 17:58:52 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,16 @@
+// We are a way for the cosmos to know itself. -- C. Sagan
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
